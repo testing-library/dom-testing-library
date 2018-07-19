7730d254339185fe11d351e7b9cd01b41cafea1b
dadamssg <adams.david.10@gmail.com>
2018-07-19 13:15:36 -0500

    fix: Reference Node and MutationObserver off of the window object rather than globally (#77)
    You have to handle shims yourself if not using jest without this

--- src/get-node-text.js
+++ src/get-node-text.js
@@ -1,7 +1,8 @@
 function getNodeText(node) {
   return Array.from(node.childNodes)
     .filter(
-      child => child.nodeType === Node.TEXT_NODE && Boolean(child.textContent),
+      child =>
+        child.nodeType === window.Node.TEXT_NODE && Boolean(child.textContent),
     )
     .map(c => c.textContent)
     .join(' ')
