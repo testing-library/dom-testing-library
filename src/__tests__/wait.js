f29f551b1d9b031b0d8cf2d2dc436eae229ecc1d
Taylor Briggs <TaylorBriggs@users.noreply.github.com>
2018-10-22 15:42:35 -0500

    fix: flaky test for `wait` (#131) (#134)
    <!--
    Thanks for your interest in the project. Bugs filed and PRs submitted are appreciated!
    
    Please make sure that you are familiar with and follow the Code of Conduct for
    this project (found in the CODE_OF_CONDUCT.md file).
    
    Also, please make sure you're familiar with and follow the instructions in the
    contributing guidelines (found in the CONTRIBUTING.md file).
    
    If you're new to contributing to open source projects, you might find this free
    video course helpful: http://kcd.im/pull-request
    
    Please fill out the information below to expedite the review and (hopefully)
    merge of your pull request!
    -->
    
    <!-- What changes are being made? (What feature/bug is being fixed here?) -->
    
    **What**:
    
    Fixes flaky test for `wait` (#131)
    
    <!-- Why are these changes necessary? -->
    
    **Why**:
    
    Random test failures prevent builds from shipping.
    
    <!-- How were these changes implemented? -->
    
    **How**:
    
    Use jest [timer mocks](https://jestjs.io/docs/en/timer-mocks).
    
    <!-- Have you done all of these things?  -->
    
    **Checklist**:
    
    <!-- add "N/A" to the end of each line that's irrelevant to your changes -->
    
    <!-- to check an item, place an "x" in the box like so: "- [x] Documentation" -->
    
    - [ ] Documentation N/A
    - [x] Tests
    - [x] Ready to be merged <!-- In your opinion, is this ready to be merged as soon as it's reviewed? -->
    - [x] Added myself to contributors table <!-- this is optional, see the contributing guidelines for instructions -->
    
    <!-- feel free to add additional comments -->

--- src/__tests__/wait.js
+++ src/__tests__/wait.js
@@ -12,14 +12,12 @@ test('it waits for the data to be loaded', async () => {
 })
 
 test('can just be used for a next tick thing', async () => {
+  jest.useFakeTimers()
   const spy = jest.fn()
   Promise.resolve().then(spy)
   expect(spy).toHaveBeenCalledTimes(0) // promises are always async
-  const before = Date.now()
   await wait() // wait for next tick
-  const after = Date.now()
-  // if it's greater than 50 then `wait` used a timeout
-  // but it should have resolved sooner. It really should be 0
-  expect(after - before).not.toBeGreaterThan(60)
+  jest.advanceTimersByTime(60)
   expect(spy).toHaveBeenCalledTimes(1)
+  jest.clearAllTimers()
 })
