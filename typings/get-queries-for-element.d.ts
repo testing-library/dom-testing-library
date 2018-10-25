a1de1f657a8b345f90bdbcbeabab2e8d0e677fce
John Gozde <john@gozde.ca>
2018-10-25 17:41:28 -0600

    fix(typings): handle double-bound functions
    fixes #135

--- typings/get-queries-for-element.d.ts
+++ typings/get-queries-for-element.d.ts
@@ -2,12 +2,15 @@ import {Matcher} from './matches'
 import * as queries from './queries'
 
 export type BoundFunction<T> = T extends (
-  a1: any,
+  attribute: string,
+  element: HTMLElement,
   text: infer P,
   options: infer Q,
 ) => infer R
   ? (text: P, options?: Q) => R
-  : never
+  : T extends (a1: any, text: infer P, options: infer Q) => infer R
+    ? (text: P, options?: Q) => R
+    : never
 export type BoundFunctions<T> = {[P in keyof T]: BoundFunction<T[P]>}
 
 export function getQueriesForElement(
