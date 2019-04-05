9d5b32dbd14e9631dadedbf9afceb09f4b868dc7
Kent C. Dodds <kent@doddsfamily.us>
2019-04-05 08:21:31 -0600

    WIP: I forgot about this... Will come back later

--- src/__tests__/get-by-errors.js
+++ src/__tests__/get-by-errors.js
@@ -42,3 +42,45 @@ cases(
     },
   },
 )
+
+cases(
+  'queryBy* queries throw an error when there are multiple elements returned',
+  ({name, query, html}) => {
+    const utils = render(html)
+    expect(() => utils[name](query)).toThrow(/multiple elements/i)
+  },
+  {
+    queryByLabelText: {
+      query: /his/,
+      html: `<div aria-label="his"></div><div aria-label="history"></div>`,
+    },
+    queryByPlaceholderText: {
+      query: /his/,
+      html: `<input placeholder="his" /><input placeholder="history" />`,
+    },
+    queryByText: {
+      query: /his/,
+      html: `<div>his</div><div>history</div>`,
+    },
+    queryByAltText: {
+      query: /his/,
+      html: `<img alt="his" src="his.png" /><img alt="history" src="history.png" />`,
+    },
+    queryByTitle: {
+      query: /his/,
+      html: `<div title="his"></div><div title="history"></div>`,
+    },
+    queryByDisplayValue: {
+      query: /his/,
+      html: `<input value="his" /><select><option value="history">history</option></select>`,
+    },
+    queryByRole: {
+      query: /his/,
+      html: `<div role="his"></div><div role="history"></div>`,
+    },
+    queryByTestId: {
+      query: /his/,
+      html: `<div data-testid="his"></div><div data-testid="history"></div>`,
+    },
+  },
+)
