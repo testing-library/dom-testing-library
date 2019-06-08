4b2e8a535d8e88bc03126307cbee48a74dbf547e
Tamás Szabó <szabototo89@gmail.com>
2019-06-08 21:45:48 +0200

    feat: add semantic html elements implicit role support to queryByRole/getByRole selectors (#262) (#280)
    * Add aria-query library as a dependency
    
    * Extend queryByRole to use aria-query ARIA roles to identify semantic HTML elements

--- src/queries/role.js
+++ src/queries/role.js
@@ -1,6 +1,74 @@
-import {queryAllByAttribute, buildQueries} from './all-utils'
+import {buildQueries, fuzzyMatches, makeNormalizer, matches} from './all-utils'
+import {elementRoles} from 'aria-query'
 
-const queryAllByRole = queryAllByAttribute.bind(null, 'role')
+function buildElementRoleList(elementRolesMap) {
+  function makeElementSelector({name, attributes = []}) {
+    return `${name}${attributes
+      .map(({name: attributeName, value}) => `[${attributeName}=${value}]`)
+      .join('')}`
+  }
+
+  function getSelectorSpecificity({attributes = []}) {
+    return attributes.length
+  }
+
+  function bySelectorSpecificity(
+    {specificity: leftSpecificity},
+    {specificity: rightSpecificity},
+  ) {
+    return rightSpecificity - leftSpecificity
+  }
+
+  let result = []
+
+  for (const [element, roles] of elementRolesMap.entries()) {
+    result = [
+      ...result,
+      {
+        selector: makeElementSelector(element),
+        roles: Array.from(roles),
+        specificity: getSelectorSpecificity(element),
+      },
+    ]
+  }
+
+  return result.sort(bySelectorSpecificity)
+}
+
+const elementRoleList = buildElementRoleList(elementRoles)
+
+function queryAllByRole(
+  container,
+  role,
+  {exact = true, collapseWhitespace, trim, normalizer} = {},
+) {
+  const matcher = exact ? matches : fuzzyMatches
+  const matchNormalizer = makeNormalizer({collapseWhitespace, trim, normalizer})
+
+  function getImplicitAriaRole(currentNode) {
+    for (const {selector, roles} of elementRoleList) {
+      if (currentNode.matches(selector)) {
+        return [...roles]
+      }
+    }
+
+    return []
+  }
+
+  return Array.from(container.querySelectorAll('*')).filter(node => {
+    const isRoleSpecifiedExplicitly = node.hasAttribute('role')
+
+    if (isRoleSpecifiedExplicitly) {
+      return matcher(node.getAttribute('role'), node, role, matchNormalizer)
+    }
+
+    const implicitRoles = getImplicitAriaRole(node)
+
+    return implicitRoles.some(implicitRole =>
+      matcher(implicitRole, node, role, matchNormalizer),
+    )
+  })
+}
 
 const getMultipleError = (c, id) => `Found multiple elements by [role=${id}]`
 const getMissingError = (c, id) => `Unable to find an element by [role=${id}]`
