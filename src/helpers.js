f53c2143553d81c56261fe3d11babd292dac7f24
Kent C. Dodds <kent@doddsfamily.us>
2019-07-11 16:26:19 -0600

    fix coverage

--- src/helpers.js
+++ src/helpers.js
@@ -2,10 +2,13 @@ import MutationObserver from '@sheerun/mutationobserver-shim'
 
 const globalObj = typeof window === 'undefined' ? global : window
 
+// we only run our tests in node, and setImmediate is supported in node.
+// istanbul ignore next
 function setImmediatePolyfill(fn) {
   return globalObj.setTimeout(fn, 0)
 }
 
+// istanbul ignore next
 const {
   setTimeout,
   clearTimeout,
