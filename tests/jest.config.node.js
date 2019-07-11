f79a2d5e2497af4dfec74010aca93a98252652de
Kent C. Dodds <kent@doddsfamily.us>
2019-07-11 16:22:10 -0600

    no longer needed

--- tests/jest.config.node.js
+++ tests/jest.config.node.js
@@ -4,7 +4,6 @@ const baseConfig = require('kcd-scripts/jest')
 module.exports = {
   ...baseConfig,
   rootDir: path.join(__dirname, '..'),
-  setupFilesAfterEnv: ['<rootDir>/tests/setup-env-node.js'],
   displayName: 'node',
   testEnvironment: 'jest-environment-node',
   testMatch: ['**/__node_tests__/**.js'],
