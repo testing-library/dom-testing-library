5bcd1a7bfe7ed962f4818952197ac9f3c9e8794e
Kent C. Dodds <kent+github@doddsfamily.us>
2019-08-09 13:40:02 -0600

    feat(logDOM): add logDOM export (#336)

--- src/pretty-dom.js
+++ src/pretty-dom.js
@@ -1,21 +1,50 @@
 import prettyFormat from 'pretty-format'
+import {getDocument} from './helpers'
+
+function inCypress(dom) {
+  const window =
+    (dom.ownerDocument && dom.ownerDocument.defaultView) || undefined
+  return (
+    (typeof global !== 'undefined' && global.Cypress) ||
+    (typeof window !== 'undefined' && window.Cypress)
+  )
+}
+
+const inNode = () =>
+  typeof process !== 'undefined' &&
+  process.versions !== undefined &&
+  process.versions.node !== undefined
+
+const getMaxLength = dom =>
+  inCypress(dom) ? 0 : process.env.DEBUG_PRINT_LIMIT || 7000
 
 const {DOMElement, DOMCollection} = prettyFormat.plugins
 
-function prettyDOM(htmlElement, maxLength, options) {
-  if (htmlElement.documentElement) {
-    htmlElement = htmlElement.documentElement
+function prettyDOM(
+  dom = getDocument().body,
+  maxLength = getMaxLength(dom),
+  options,
+) {
+  if (maxLength === 0) {
+    return ''
+  }
+  if (dom.documentElement) {
+    dom = dom.documentElement
   }
 
-  const debugContent = prettyFormat(htmlElement, {
+  const debugContent = prettyFormat(dom, {
     plugins: [DOMElement, DOMCollection],
     printFunctionName: false,
-    highlight: true,
+    highlight: inNode(),
     ...options,
   })
-  return maxLength !== undefined && htmlElement.outerHTML.length > maxLength
+  return maxLength !== undefined && dom.outerHTML.length > maxLength
     ? `${debugContent.slice(0, maxLength)}...`
     : debugContent
 }
 
-export {prettyDOM}
+const logDOM = (...args) => console.log(prettyDOM(...args))
+
+export {prettyDOM, logDOM}
+
+/* eslint no-console:0 */
