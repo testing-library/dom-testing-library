1bd90b1e4dd2cf24a64214e0c1df6910c61a454e
Andrew Hill <andrew.hill@intellihr.co>
2019-08-17 08:32:05 +1000

    feat: add asyncUtilTimeout to config (#339)

--- src/wait.js
+++ src/wait.js
@@ -1,7 +1,7 @@
 import waitForExpect from 'wait-for-expect'
 import {getConfig} from './config'
 
-function wait(callback = () => {}, {timeout = 4500, interval = 50} = {}) {
+function wait(callback = () => {}, {timeout = getConfig().asyncUtilTimeout, interval = 50} = {}) {
   return waitForExpect(callback, timeout, interval)
 }
 
