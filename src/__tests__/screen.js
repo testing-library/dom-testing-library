1611b06a5dfb5d8608068082a1662b509a7fe7be
josh-dowdle <joshua.dowdle@lifeomic.com>
2020-01-21 14:29:10 -0700

    Add a debug method to screen.

--- src/__tests__/screen.js
+++ src/__tests__/screen.js
@@ -1,9 +1,63 @@
 import {renderIntoDocument} from './helpers/test-utils'
 import {screen} from '..'
 
+beforeEach(() => {
+  jest.spyOn(console, 'log').mockImplementation(() => {})
+})
+
+afterEach(() => {
+  console.log.mockRestore()
+})
+
 test('exposes queries that are attached to document.body', async () => {
   renderIntoDocument(`<div>hello world</div>`)
   screen.getByText(/hello world/i)
   await screen.findByText(/hello world/i)
   expect(screen.queryByText(/hello world/i)).not.toBeNull()
 })
+
+test('exposes debug method', () => {
+  renderIntoDocument(
+    `<button>test</button><span>multi-test</span><div>multi-test</div>`,
+  )
+  expect(screen.debug).toBeInstanceOf(Function)
+  // log document
+  screen.debug()
+  expect(console.log).toHaveBeenCalledTimes(1)
+  expect(console.log.mock.calls[0][0]).toMatchInlineSnapshot(`
+    "<body>
+      <button>
+        test
+      </button>
+      <span>
+        multi-test
+      </span>
+      <div>
+        multi-test
+      </div>
+    </body>"
+  `)
+  // log single element
+  screen.debug(screen.getByText('test', {selector: 'button'}))
+  expect(console.log).toHaveBeenCalledTimes(2)
+  expect(console.log.mock.calls[1][0]).toMatchInlineSnapshot(`
+    "<button>
+      test
+    </button>"
+  `)
+  // log multiple elements
+  screen.debug(screen.getAllByText('multi-test'))
+  expect(console.log).toHaveBeenCalledTimes(4)
+  expect(console.log.mock.calls[2][0]).toMatchInlineSnapshot(`
+    "<span>
+      multi-test
+    </span>"
+  `)
+  expect(console.log.mock.calls[3][0]).toMatchInlineSnapshot(`
+    "<div>
+      multi-test
+    </div>"
+  `)
+})
+
+/* eslint no-console:0 */
--- src/screen.js
+++ src/screen.js
@@ -1,14 +1,23 @@
 import * as queries from './queries'
 import {getQueriesForElement} from './get-queries-for-element'
+import {logDOM} from './pretty-dom'
+
+const debug = (element, maxLength, options) =>
+  Array.isArray(element)
+    ? element.forEach(el => logDOM(el, maxLength, options))
+    : logDOM(element, maxLength, options)
 
 export const screen =
   typeof document !== 'undefined' && document.body
-    ? getQueriesForElement(document.body)
-    : Object.keys(queries).reduce((helpers, key) => {
-        helpers[key] = () => {
-          throw new TypeError(
-            'For queries bound to document.body a global document has to be available... Learn more: https://testing-library.com/s/screen-global-error',
-          )
-        }
-        return helpers
-      }, {})
+    ? getQueriesForElement(document.body, queries, {debug})
+    : Object.keys(queries).reduce(
+        (helpers, key) => {
+          helpers[key] = () => {
+            throw new TypeError(
+              'For queries bound to document.body a global document has to be available... Learn more: https://testing-library.com/s/screen-global-error',
+            )
+          }
+          return helpers
+        },
+        {debug},
+      )
