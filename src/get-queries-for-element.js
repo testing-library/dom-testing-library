3805b2772f93d2e5a16858d50231eeafb6c6e484
Josh <jkdowdle@users.noreply.github.com>
2020-01-28 15:39:31 -0700

    feat: add a debug method to screen. (#429)
    * Add a debug method to screen.
    
    * Make suggested edits.

--- src/get-queries-for-element.js
+++ src/get-queries-for-element.js
@@ -7,14 +7,19 @@ import * as defaultQueries from './queries'
 /**
  * @param {HTMLElement} element container
  * @param {FuncMap} queries object of functions
+ * @param {Object} initialValue for reducer
  * @returns {FuncMap} returns object of functions bound to container
  */
-function getQueriesForElement(element, queries = defaultQueries) {
+function getQueriesForElement(
+  element,
+  queries = defaultQueries,
+  initialValue = {},
+) {
   return Object.keys(queries).reduce((helpers, key) => {
     const fn = queries[key]
     helpers[key] = fn.bind(null, element)
     return helpers
-  }, {})
+  }, initialValue)
 }
 
 export {getQueriesForElement}
