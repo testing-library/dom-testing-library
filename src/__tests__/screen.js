d64f32b20c2f2870b466b25c2dd53bed67f49272
josh-dowdle <joshua.dowdle@lifeomic.com>
2020-01-28 14:25:58 -0700

    Make suggested edits.

--- src/__tests__/screen.js
+++ src/__tests__/screen.js
@@ -20,7 +20,6 @@ test('exposes debug method', () => {
   renderIntoDocument(
     `<button>test</button><span>multi-test</span><div>multi-test</div>`,
   )
-  expect(screen.debug).toBeInstanceOf(Function)
   // log document
   screen.debug()
   expect(console.log).toHaveBeenCalledTimes(1)
@@ -37,27 +36,30 @@ test('exposes debug method', () => {
       </div>
     </body>"
   `)
+  console.log.mockClear()
   // log single element
   screen.debug(screen.getByText('test', {selector: 'button'}))
-  expect(console.log).toHaveBeenCalledTimes(2)
-  expect(console.log.mock.calls[1][0]).toMatchInlineSnapshot(`
+  expect(console.log).toHaveBeenCalledTimes(1)
+  expect(console.log.mock.calls[0][0]).toMatchInlineSnapshot(`
     "<button>
       test
     </button>"
   `)
+  console.log.mockClear()
   // log multiple elements
   screen.debug(screen.getAllByText('multi-test'))
-  expect(console.log).toHaveBeenCalledTimes(4)
-  expect(console.log.mock.calls[2][0]).toMatchInlineSnapshot(`
+  expect(console.log).toHaveBeenCalledTimes(2)
+  expect(console.log.mock.calls[0][0]).toMatchInlineSnapshot(`
     "<span>
       multi-test
     </span>"
   `)
-  expect(console.log.mock.calls[3][0]).toMatchInlineSnapshot(`
+  expect(console.log.mock.calls[1][0]).toMatchInlineSnapshot(`
     "<div>
       multi-test
     </div>"
   `)
+  console.log.mockClear()
 })
 
 /* eslint no-console:0 */
