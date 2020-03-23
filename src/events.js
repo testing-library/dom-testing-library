37815d5224537a50dff35c32289b9cf38fe61d9d
Adam Palaniuk <Adam.Palaniuk@D2L.com>
2020-03-23 12:57:42 -0500

    fix: set composed init option for relevant UI events

--- src/events.js
+++ src/events.js
@@ -3,58 +3,58 @@ const eventMap = {
   // Clipboard Events
   copy: {
     EventType: 'ClipboardEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   cut: {
     EventType: 'ClipboardEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   paste: {
     EventType: 'ClipboardEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   // Composition Events
   compositionEnd: {
     EventType: 'CompositionEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   compositionStart: {
     EventType: 'CompositionEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   compositionUpdate: {
     EventType: 'CompositionEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   // Keyboard Events
   keyDown: {
     EventType: 'KeyboardEvent',
-    defaultInit: {bubbles: true, cancelable: true, charCode: 0},
+    defaultInit: {bubbles: true, cancelable: true, charCode: 0, composed: true},
   },
   keyPress: {
     EventType: 'KeyboardEvent',
-    defaultInit: {bubbles: true, cancelable: true, charCode: 0},
+    defaultInit: {bubbles: true, cancelable: true, charCode: 0, composed: true},
   },
   keyUp: {
     EventType: 'KeyboardEvent',
-    defaultInit: {bubbles: true, cancelable: true, charCode: 0},
+    defaultInit: {bubbles: true, cancelable: true, charCode: 0, composed: true},
   },
   // Focus Events
   focus: {
     EventType: 'FocusEvent',
-    defaultInit: {bubbles: false, cancelable: false},
+    defaultInit: {bubbles: false, cancelable: false, composed: true},
   },
   blur: {
     EventType: 'FocusEvent',
-    defaultInit: {bubbles: false, cancelable: false},
+    defaultInit: {bubbles: false, cancelable: false, composed: true},
   },
   focusIn: {
     EventType: 'FocusEvent',
-    defaultInit: {bubbles: true, cancelable: false},
+    defaultInit: {bubbles: true, cancelable: false, composed: true},
   },
   focusOut: {
     EventType: 'FocusEvent',
-    defaultInit: {bubbles: true, cancelable: false},
+    defaultInit: {bubbles: true, cancelable: false, composed: true},
   },
   // Form Events
   change: {
@@ -63,7 +63,7 @@ const eventMap = {
   },
   input: {
     EventType: 'InputEvent',
-    defaultInit: {bubbles: true, cancelable: false},
+    defaultInit: {bubbles: true, cancelable: false, composed: true},
   },
   invalid: {
     EventType: 'Event',
@@ -80,75 +80,75 @@ const eventMap = {
   // Mouse Events
   click: {
     EventType: 'MouseEvent',
-    defaultInit: {bubbles: true, cancelable: true, button: 0},
+    defaultInit: {bubbles: true, cancelable: true, button: 0, composed: true},
   },
   contextMenu: {
     EventType: 'MouseEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   dblClick: {
     EventType: 'MouseEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   drag: {
     EventType: 'DragEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   dragEnd: {
     EventType: 'DragEvent',
-    defaultInit: {bubbles: true, cancelable: false},
+    defaultInit: {bubbles: true, cancelable: false, composed: true},
   },
   dragEnter: {
     EventType: 'DragEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   dragExit: {
     EventType: 'DragEvent',
-    defaultInit: {bubbles: true, cancelable: false},
+    defaultInit: {bubbles: true, cancelable: false, composed: true},
   },
   dragLeave: {
     EventType: 'DragEvent',
-    defaultInit: {bubbles: true, cancelable: false},
+    defaultInit: {bubbles: true, cancelable: false, composed: true},
   },
   dragOver: {
     EventType: 'DragEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   dragStart: {
     EventType: 'DragEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   drop: {
     EventType: 'DragEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   mouseDown: {
     EventType: 'MouseEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   mouseEnter: {
     EventType: 'MouseEvent',
-    defaultInit: {bubbles: false, cancelable: false},
+    defaultInit: {bubbles: false, cancelable: false, composed: true},
   },
   mouseLeave: {
     EventType: 'MouseEvent',
-    defaultInit: {bubbles: false, cancelable: false},
+    defaultInit: {bubbles: false, cancelable: false, composed: true},
   },
   mouseMove: {
     EventType: 'MouseEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   mouseOut: {
     EventType: 'MouseEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   mouseOver: {
     EventType: 'MouseEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   mouseUp: {
     EventType: 'MouseEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   // Selection Events
   select: {
@@ -158,19 +158,19 @@ const eventMap = {
   // Touch Events
   touchCancel: {
     EventType: 'TouchEvent',
-    defaultInit: {bubbles: true, cancelable: false},
+    defaultInit: {bubbles: true, cancelable: false, composed: true},
   },
   touchEnd: {
     EventType: 'TouchEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   touchMove: {
     EventType: 'TouchEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   touchStart: {
     EventType: 'TouchEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   // UI Events
   scroll: {
@@ -180,7 +180,7 @@ const eventMap = {
   // Wheel Events
   wheel: {
     EventType: 'WheelEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   // Media Events
   abort: {
@@ -301,35 +301,35 @@ const eventMap = {
   // pointer events
   pointerOver: {
     EventType: 'PointerEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   pointerEnter: {
     EventType: 'PointerEvent',
-    defaultInit: {bubbles: false, cancelable: false},
+    defaultInit: {bubbles: false, cancelable: false, composed: true},
   },
   pointerDown: {
     EventType: 'PointerEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   pointerMove: {
     EventType: 'PointerEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   pointerUp: {
     EventType: 'PointerEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   pointerCancel: {
     EventType: 'PointerEvent',
-    defaultInit: {bubbles: true, cancelable: false},
+    defaultInit: {bubbles: true, cancelable: false, composed: true},
   },
   pointerOut: {
     EventType: 'PointerEvent',
-    defaultInit: {bubbles: true, cancelable: true},
+    defaultInit: {bubbles: true, cancelable: true, composed: true},
   },
   pointerLeave: {
     EventType: 'PointerEvent',
-    defaultInit: {bubbles: false, cancelable: false},
+    defaultInit: {bubbles: false, cancelable: false, composed: true},
   },
   gotPointerCapture: {
     EventType: 'PointerEvent',
