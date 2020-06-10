65ef08094cd603b3c761ef74286c9f2d6b69a129
Vincent Ricard <magic@magicninja.org>
2020-06-10 18:28:41 +0200

    Add the 'types' directory into .prettierignore
    It seems the prettier settings can conflit with the 'whitespace' rule of dtslint.
    
    Also forces the dtslint version to be sure Travis use a version with this fix: https://github.com/microsoft/dtslint/pull/295

--- tests/jest.config.dom.js
+++ tests/jest.config.dom.js
@@ -11,10 +11,4 @@ module.exports = {
     '/__node_tests__/',
   ],
   testEnvironment: 'jest-environment-jsdom',
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': path.join(
-      __dirname,
-      '../node_modules/kcd-scripts/dist/config/babel-transform',
-    ),
-  },
 }
