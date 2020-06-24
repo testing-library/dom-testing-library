86205e5ba38451e81f96ed021f53be9f3b716a3e
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2020-06-24 15:51:03 +0200

    fix: add getConfig to types (#668)

--- types/config.d.ts
+++ types/config.d.ts
@@ -1,14 +1,15 @@
 export interface Config {
-    testIdAttribute: string;
-    asyncWrapper(cb: (...args: any[]) => any): Promise<any>;
-    eventWrapper(cb: (...args: any[]) => any): void;
-    asyncUtilTimeout: number;
-    defaultHidden: boolean;
-    throwSuggestions: boolean;
+  testIdAttribute: string
+  asyncWrapper(cb: (...args: any[]) => any): Promise<any>
+  eventWrapper(cb: (...args: any[]) => any): void
+  asyncUtilTimeout: number
+  defaultHidden: boolean
+  throwSuggestions: boolean
 }
 
 export interface ConfigFn {
-    (existingConfig: Config): Partial<Config>;
+  (existingConfig: Config): Partial<Config>
 }
 
-export function configure(configDelta: Partial<Config> | ConfigFn): void;
+export function configure(configDelta: Partial<Config> | ConfigFn): void
+export function getConfig(): Config
