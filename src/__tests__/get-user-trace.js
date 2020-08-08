fe31e3460e8b40d45faf46b02b42e1f2981165c7
Victor Cordova <azeta351@gmail.com>
2020-08-08 19:51:30 +0200

    feat(get-user-trace): create utility to obtain the client's stack frame for debugging

---
+++ src/__tests__/get-user-trace.js
@@ -0,0 +1,43 @@
+import {getUserTrace} from '../get-user-trace'
+
+let globalErrorMock
+
+beforeEach(() => {
+  // Mock global.Error so we can setup our own stack messages
+  globalErrorMock = jest.spyOn(global, 'Error')
+})
+
+afterEach(() => {
+  global.Error.mockRestore()
+})
+
+test('it returns only client error when frames from node_modules are first', () => {
+  const stack = `Error: Kaboom
+      at Object.<anonymous> (/home/john/projects/projects/sample-error/node_modules/@es2050/console/build/index.js:4:10)
+      at somethingWrong (/home/john/projects/sample-error/error-example.js:2:13)
+  `
+  globalErrorMock.mockImplementationOnce(() => ({stack}))
+  const userTrace = getUserTrace(stack)
+  expect(userTrace).toEqual(
+    '/home/john/projects/sample-error/error-example.js:2:13\n',
+  )
+})
+
+test('it returns only client error when node frames are present afterwards', () => {
+  const stack = `Error: Kaboom
+      at Object.<anonymous> (/home/john/projects/projects/sample-error/node_modules/@es2050/console/build/index.js:4:10)
+      at somethingWrong (/home/john/projects/sample-error/error-example.js:2:13)
+      at Object.<anonymous> (/home/user/Documents/projects/sample-error/error-example.js:14:1)
+      at Module._compile (internal/modules/cjs/loader.js:1151:30)
+      at Object.Module._extensions..js (internal/modules/cjs/loader.js:1171:10)
+      at Module.load (internal/modules/cjs/loader.js:1000:32)
+      at Function.Module._load (internal/modules/cjs/loader.js:899:14)
+      at Function.executeUserEntryPoint [as runMain] (internal/modules/run_main.js:71:12)
+      at internal/main/run_main_module.js:17:47
+  `
+  globalErrorMock.mockImplementationOnce(() => ({stack}))
+  const userTrace = getUserTrace()
+  expect(userTrace).toEqual(
+    '/home/john/projects/sample-error/error-example.js:2:13\n',
+  )
+})
---
+++ src/get-user-trace.js
@@ -0,0 +1,19 @@
+// Frame has the form "at myMethod (location/to/my/file.js:10:2)"
+function getFrameLocation(frame) {
+  const locationStart = frame.indexOf('(') + 1
+  const locationEnd = frame.indexOf(')')
+
+  return frame.slice(locationStart, locationEnd)
+}
+
+function getUserTrace() {
+  const err = new Error()
+  const firstClientCodeFrame = err.stack
+    .split('\n')
+    .slice(1) // Remove first line which has the form "Error: TypeError"
+    .find(frame => !frame.includes('node_modules/')) // Ignore frames from 3rd party libraries
+
+  return `${getFrameLocation(firstClientCodeFrame)}\n`
+}
+
+export {getUserTrace}
