68004ee31d00675d027bef4d9a4f5abbd50accea
marcosvega91 <marcomoretti0103@gmail.com>
2020-08-20 10:54:57 +0200

    test: check warning message in test

--- src/__tests__/suggestions.js
+++ src/__tests__/suggestions.js
@@ -8,6 +8,7 @@ beforeAll(() => {
 
 afterEach(() => {
   configure({testIdAttribute: 'data-testid'})
+  console.warn.mockClear()
 })
 
 afterAll(() => {
@@ -562,5 +563,18 @@ test('should suggest hidden option if element is not in the accessibilty tree', 
     queryMethod: 'getByRole',
     queryArgs: ['textbox', {hidden: true}],
     variant: 'get',
+    warning: `Element is inaccessible. This means that the element and all its children are invisible to screen readers.
+    If you are using the aria-hidden prop, make sure this is the right choice for your case.
+    `,
   })
+
+  expect(console.warn.mock.calls).toMatchInlineSnapshot(`
+    Array [
+      Array [
+        "Element is inaccessible. This means that the element and all its children are invisible to screen readers.
+        If you are using the aria-hidden prop, make sure this is the right choice for your case.
+        ",
+      ],
+    ]
+  `)
 })
