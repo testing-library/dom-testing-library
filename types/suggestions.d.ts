6d6641d17ad808ba068e58b4119b2f1b64c0f4ac
Marco Moretti <marcomoretti0103@gmail.com>
2020-08-23 00:56:30 +0200

    fix: suggest hidden:true with getByRole if element is inaccessible  (#745)

--- types/suggestions.d.ts
+++ types/suggestions.d.ts
@@ -1,5 +1,15 @@
+export interface QueryOptions {
+  [key: string]: RegExp | boolean
+}
+
+export type QueryArgs = [string, QueryOptions?]
+
 export interface Suggestion {
   queryName: string
+  queryMethod: string
+  queryArgs: QueryArgs
+  variant: string
+  warning?: string
   toString(): string
 }
 
