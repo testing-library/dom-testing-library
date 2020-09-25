68f8f81d433d7c647a1daf5797feb8384c539358
Dmitry Semigradsky <semigradskyd@gmail.com>
2020-09-25 11:48:53 +0300

    fix: Mark deprecated methods as `@deprecated` (#771)

--- types/wait.d.ts
+++ types/wait.d.ts
@@ -1,3 +1,8 @@
+/**
+ * @deprecated `wait` has been deprecated and replaced by `waitFor` instead.
+ * In most cases you should be able to find/replace `wait` with `waitFor`.
+ * Learn more: https://testing-library.com/docs/dom-testing-library/api-async#waitfor.
+ */
 export function wait(
     callback?: () => void,
     options?: {
