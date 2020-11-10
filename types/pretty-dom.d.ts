e32937ce4eb5b5f26ffdac5eeb82d62e0ac05476
Marco Moretti <marcomoretti0103@gmail.com>
2020-11-10 19:06:55 +0100

    fix: export pretty format (#818)

--- types/pretty-dom.d.ts
+++ types/pretty-dom.d.ts
@@ -1,4 +1,13 @@
-import { OptionsReceived } from 'pretty-format';
+import * as prettyFormat from 'pretty-format'
 
-export function prettyDOM(dom?: Element | HTMLDocument, maxLength?: number, options?: OptionsReceived): string | false;
-export function logDOM(dom?: Element | HTMLDocument, maxLength?: number, options?: OptionsReceived): void;
+export function prettyDOM(
+  dom?: Element | HTMLDocument,
+  maxLength?: number,
+  options?: prettyFormat.OptionsReceived,
+): string | false
+export function logDOM(
+  dom?: Element | HTMLDocument,
+  maxLength?: number,
+  options?: prettyFormat.OptionsReceived,
+): void
+export {prettyFormat}
