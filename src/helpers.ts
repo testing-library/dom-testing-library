b6bc17f4493c00a5f462a98985262ed8adccca64
marcosvega91 <marcomoretti0103@gmail.com>
2020-12-17 11:48:28 +0100

    feat: migrate helpers.ts and pretty-dom.ts

--- src/helpers.ts
+++ src/helpers.ts
@@ -4,7 +4,7 @@ const globalObj = typeof window === 'undefined' ? global : window
 const TEXT_NODE = 3
 
 // Currently this fn only supports jest timers, but it could support other test runners in the future.
-function runWithRealTimers(callback) {
+function runWithRealTimers<T>(callback: () => T) {
   const fakeTimersType = getJestFakeTimersType()
   if (fakeTimersType) {
     jest.useRealTimers()
@@ -29,13 +29,16 @@ function getJestFakeTimersType() {
   }
 
   if (
+    // @ts-expect-error check if we are using jest.fakeTimers('legacy')
     typeof globalObj.setTimeout._isMockFunction !== 'undefined' &&
+    // @ts-expect-error check if we are using jest.fakeTimers('legacy')
     globalObj.setTimeout._isMockFunction
   ) {
     return 'legacy'
   }
 
   if (
+    // @ts-expect-error check if we are using jest.fakeTimers('modern')
     typeof globalObj.setTimeout.clock !== 'undefined' &&
     typeof jest.getRealSystemTime !== 'undefined'
   ) {
@@ -54,7 +57,7 @@ const jestFakeTimersAreEnabled = () => Boolean(getJestFakeTimersType())
 
 // we only run our tests in node, and setImmediate is supported in node.
 // istanbul ignore next
-function setImmediatePolyfill(fn) {
+function setImmediatePolyfill(fn: () => void) {
   return globalObj.setTimeout(fn, 0)
 }
 
@@ -62,6 +65,7 @@ function getTimeFunctions() {
   // istanbul ignore next
   return {
     clearTimeoutFn: globalObj.clearTimeout,
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- for browser compatibility
     setImmediateFn: globalObj.setImmediate || setImmediatePolyfill,
     setTimeoutFn: globalObj.setTimeout,
   }
@@ -78,16 +82,17 @@ function getDocument() {
   }
   return window.document
 }
-function getWindowFromNode(node) {
-  if (node.defaultView) {
+function getWindowFromNode(node: Document | Node | Window) {
+  if ('defaultView' in node) {
     // node is document
     return node.defaultView
-  } else if (node.ownerDocument && node.ownerDocument.defaultView) {
+  } else if ('ownerDocument' in node && node.ownerDocument?.defaultView) {
     // node is a DOM node
     return node.ownerDocument.defaultView
-  } else if (node.window) {
+  } else if ('window' in node) {
     // node is window
     return node.window
+    // @ts-expect-error in case the use pass a Promise, we want to provide a specific message for this case
   } else if (node.then instanceof Function) {
     throw new Error(
       `It looks like you passed a Promise object instead of a DOM node. Did you do something like \`fireEvent.click(screen.findBy...\` when you meant to use a \`getBy\` query \`fireEvent.click(screen.getBy...\`, or await the findBy query \`fireEvent.click(await screen.findBy...\`?`,
@@ -100,7 +105,7 @@ function getWindowFromNode(node) {
   }
 }
 
-function checkContainerType(container) {
+function checkContainerType(container?: Element) {
   if (
     !container ||
     !(typeof container.querySelector === 'function') ||
@@ -113,9 +118,9 @@ function checkContainerType(container) {
     )
   }
 
-  function getTypeName(object) {
+  function getTypeName<T>(object: T) {
     if (typeof object === 'object') {
-      return object === null ? 'null' : object.constructor.name
+      return object === null ? 'null' : (object as Object).constructor.name
     }
     return typeof object
   }
