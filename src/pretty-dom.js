13f4714a0fafff1aa9fe4bf21449a59474768b8d
eps1lon <silbermann.sebastian@gmail.com>
2021-04-25 12:29:50 +0200

    Use a fork of DOMElement plugin

--- src/pretty-dom.js
+++ src/pretty-dom.js
@@ -1,6 +1,8 @@
 import prettyFormat from 'pretty-format'
+import createDOMElementFilter from './DOMElementFilter'
 import {getUserCodeFrame} from './get-user-code-frame'
 import {getDocument} from './helpers'
+import {DEFAULT_IGNORE_TAGS} from './shared'
 
 function inCypress(dom) {
   const window =
@@ -21,13 +23,19 @@ const getMaxLength = dom =>
     ? 0
     : (typeof process !== 'undefined' && process.env.DEBUG_PRINT_LIMIT) || 7000
 
-const {DOMElement, DOMCollection} = prettyFormat.plugins
+const {DOMCollection} = prettyFormat.plugins
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType#node_type_constants
+const ELEMENT_NODE = 1
 const COMMENT_NODE = 8
 
 // https://github.com/facebook/jest/blob/615084195ae1ae61ddd56162c62bbdda17587569/packages/pretty-format/src/plugins/DOMElement.ts#L50
-function filterCommentsAndScriptTags(value) {
-  return value.nodeType !== COMMENT_NODE && value.tagName !== 'SCRIPT'
+function filterCommentsAndDefaultIgnoreTagsTags(value) {
+  return (
+    value.nodeType !== COMMENT_NODE &&
+    // value.nodeType === ELEMENT_NODE => !value.matches(DEFAULT_IGNORE_TAGS)
+    (value.nodeType !== ELEMENT_NODE || !value.matches(DEFAULT_IGNORE_TAGS))
+  )
 }
 
 function prettyDOM(dom, maxLength, options = {}) {
@@ -59,21 +67,12 @@ function prettyDOM(dom, maxLength, options = {}) {
   }
 
   const {
-    filterNode = filterCommentsAndScriptTags,
+    filterNode = filterCommentsAndDefaultIgnoreTagsTags,
     ...prettyFormatOptions
   } = options
 
-  const DOMElementFilter = {
-    test(value) {
-      return DOMElement.test(value) && !filterNode(value)
-    },
-    serialize() {
-      return ''
-    },
-  }
-
   const debugContent = prettyFormat(dom, {
-    plugins: [DOMElementFilter, DOMElement, DOMCollection],
+    plugins: [createDOMElementFilter(filterNode), DOMCollection],
     printFunctionName: false,
     highlight: inNode(),
     ...prettyFormatOptions,
