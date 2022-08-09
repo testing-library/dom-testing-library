18389908ad9a2438719c7286d00f86f7dbfdddd8
eps1lon <silbermann.sebastian@gmail.com>
2022-08-09 16:19:53 +0200

    chore: Convert `helpers.js` to TypeScript

--- src/helpers.ts
+++ src/helpers.ts
@@ -7,7 +7,7 @@ function jestFakeTimersAreEnabled() {
   if (typeof jest !== 'undefined' && jest !== null) {
     return (
       // legacy timers
-      setTimeout._isMockFunction === true ||
+      (setTimeout as any)._isMockFunction === true ||
       // modern timers
       Object.prototype.hasOwnProperty.call(setTimeout, 'clock')
     )
@@ -23,7 +23,7 @@ function getDocument() {
   }
   return window.document
 }
-function getWindowFromNode(node) {
+function getWindowFromNode(node: any) {
   if (node.defaultView) {
     // node is document
     return node.defaultView
@@ -60,11 +60,11 @@ function getWindowFromNode(node) {
   }
 }
 
-function checkContainerType(container) {
+function checkContainerType(container: unknown) {
   if (
     !container ||
-    !(typeof container.querySelector === 'function') ||
-    !(typeof container.querySelectorAll === 'function')
+    !(typeof (container as any).querySelector === 'function') ||
+    !(typeof (container as any).querySelectorAll === 'function')
   ) {
     throw new TypeError(
       `Expected container to be an Element, a Document or a DocumentFragment but got ${getTypeName(
@@ -73,7 +73,7 @@ function checkContainerType(container) {
     )
   }
 
-  function getTypeName(object) {
+  function getTypeName(object: unknown) {
     if (typeof object === 'object') {
       return object === null ? 'null' : object.constructor.name
     }
