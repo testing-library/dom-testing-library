ab0f71a88db1b454fa8252f0949348035d37fc1c
eps1lon <silbermann.sebastian@gmail.com>
2023-03-08 12:01:44 +0100

    Check we actually suggest

--- src/__tests__/suggestions.js
+++ src/__tests__/suggestions.js
@@ -109,7 +109,7 @@ test('should suggest when suggest is turned on for a specific query but disabled
   <button data-testid="foo">submit</button>
   <button data-testid="foot">another</button>`)
 
-  expect(() => screen.getByTestId('foo', {suggest: true})).toThrowError()
+  expect(() => screen.getByTestId('foo', {suggest: true})).toThrowError("try this:\ngetByRole('button', { name: /submit/i })")
 })
 
 test('should suggest getByRole when used with getBy', () => {
